30e5f1e80aca1040b49afae3787bcb2d50b14977
Mikhail Grachev <work@mgrachev.com>
2019-12-06 23:14:34 +0300

    Update entrypoint
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 find . -name "Dockerfile*" | xargs hadolint \
-  | /bin/reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | /bin/reviewdog -efm="%f:%l %m" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"

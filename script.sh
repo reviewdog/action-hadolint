14204d9b8cd7e54305389a527c9fd09e9be99e6d
Mikhail Grachev <work@mgrachev.com>
2021-01-09 15:25:54 +0300

    Migrate from docker to composite run actions
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- script.sh
+++ script.sh
@@ -2,7 +2,20 @@
 
 # shellcheck disable=SC2086,SC2089,SC2090
 
-cd "$GITHUB_WORKSPACE" || exit
+cd "${GITHUB_WORKSPACE}" || exit
+
+TEMP_PATH="$(mktemp -d)"
+PATH="${TEMP_PATH}:$PATH"
+
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+echo '::endgroup::'
+
+echo '::group:: Installing hadolint ... https://github.com/hadolint/hadolint'
+wget -q https://github.com/hadolint/hadolint/releases/download/$HADOLINT_VERSION/hadolint-Linux-x86_64 -O $TEMP_PATH/hadolint \
+    && chmod +x $TEMP_PATH/hadolint
+echo '::endgroup::'
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 EXCLUDES=""
@@ -17,6 +30,7 @@ done
 
 INPUT_HADOLINT_FLAGS="$INPUT_HADOLINT_FLAGS $IGNORE_LIST"
 
+echo '::group:: Running hadolint with reviewdog 🐶 ...'
 git ls-files --exclude='*Dockerfile*' --ignored ${EXCLUDES} \
   | xargs hadolint ${INPUT_HADOLINT_FLAGS} \
   | reviewdog -efm="%f:%l %m" \
@@ -26,3 +40,4 @@ git ls-files --exclude='*Dockerfile*' --ignored ${EXCLUDES} \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
     ${INPUT_REVIEWDOG_FLAGS}
+echo '::endgroup::'
